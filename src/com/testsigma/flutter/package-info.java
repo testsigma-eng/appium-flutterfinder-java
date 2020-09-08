e4f73391602a03fdcc6f5db375c2b53f3ece92a5
Renju Jose <renjujv@gmail.com>
2020-09-08 22:26:07 +0530

    First commit
    Added details in package-info

--- src/com/testsigma/flutter/package-info.java
+++ src/com/testsigma/flutter/package-info.java
@@ -6,7 +6,7 @@ package com.testsigma.flutter;
  * from kotlin code for the same from
  * @link https://github.com/truongsinh/appium-flutter-driver
  *
- * Used some third party classes - Gson, guava, and selenium - appium libs
+ * More locator mechanisms to be added later
  *
  * @since 0.1
  * @author renju
